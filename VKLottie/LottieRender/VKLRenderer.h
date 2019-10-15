dc7111231463542b5dd1af7bcf0621e7bd1fccac
Anton Sergeev <antonsergeev88@gmail.com>
2019-10-15 20:05:23 +0300

    Add lottie renderer

---
+++ VKLottie/LottieRender/VKLRenderer.h
@@ -0,0 +1,16 @@
+//
+//  VKLRenderer.h
+//  VKLottie
+//
+//  Created by Антон Сергеев on 15.10.2019.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VKLRenderer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ VKLottie/LottieRender/VKLRenderer.mm
@@ -0,0 +1,12 @@
+//
+//  VKLRenderer.m
+//  VKLottie
+//
+//  Created by Антон Сергеев on 15.10.2019.
+//
+
+#import "VKLRenderer.h"
+
+@implementation VKLRenderer
+
+@end
