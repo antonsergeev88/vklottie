96b558bd93b35a883e5841ed5d3dbd60922db182
Anton Sergeev <antonsergeev88@gmail.com>
2019-10-15 18:25:34 +0300

    Initial Commit

---
+++ VKLottie/VKLottie.h
@@ -0,0 +1,18 @@
+//
+//  VKLottie.h
+//  VKLottie
+//
+//  Created by Антон Сергеев on 15.10.2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VKLottie.
+FOUNDATION_EXPORT double VKLottieVersionNumber;
+
+//! Project version string for VKLottie.
+FOUNDATION_EXPORT const unsigned char VKLottieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKLottie/PublicHeader.h>
+
+
