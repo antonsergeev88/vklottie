cb4e2fe698333efc496dfaef29fd091839fd169b
Anton Sergeev <antonsergeev88@gmail.com>
2019-10-15 19:39:56 +0300

    Add metal view

--- VKLottie/View/VKLView.m
+++ VKLottie/View/VKLView.m
@@ -6,7 +6,31 @@
 //
 
 #import "VKLView.h"
+#import <MetalKit/MetalKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VKLView ()
+
+@property (nonatomic, readonly, strong) MTKView *animationView;
+
+@end
+
+NS_ASSUME_NONNULL_END
 
 @implementation VKLView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        _animationView = [[MTKView alloc] init];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    CGSize size = self.bounds.size;
+    self.animationView.frame = CGRectMake(0.0, 0.0, size.width, size.height);
+}
 
 @end
